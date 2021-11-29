f123e8c87f848863b4053a5c63e9ec6cdceea1e0
A-Viral-dev <aviralshivpuri@gmail.com>
2021-11-29 17:51:42 +0530

    add five round loop

--- script.js
+++ script.js
@@ -2,31 +2,21 @@ let log = n => {
   console.log(n);
 };
 // Create an array of three results
-let resultArr = ["rock", "paper", "scissors"];
-// create a function which gives an item from above array randomly using math.random
+const resultArr = ["rock", "paper", "scissors"];
+// 1. Selection
 
-let randomResult = () => {
+// 1.1 computer slects
+let compSelection = () => {
   let index = Math.floor(Math.random() * 3);
   return resultArr[index];
 };
 
-// call above function and assign to a variable
-compSelection = randomResult();
-
-// log the variable
-
-log(compSelection);
-
-// Ask the player for his selection
-let playerSelection = "";
-playerSelection = prompt(
-  "Please choose your hand!!",
-  "rock || paper || scissors"
-);
-
-// convert to lower case
-playerSelection = playerSelection.toLowerCase();
-log(playerSelection);
+// 1.2   player selects
+let playerSelection = round =>
+  prompt(
+    `Round ${round} \r\nPlease choose your hand!!`,
+    "rock || paper || scissors"
+  ).toLowerCase();
 
 // write game logic which takes both selection
 let gameLogic = (playerSelection, compSelection) => {
@@ -44,14 +34,14 @@ let gameLogic = (playerSelection, compSelection) => {
     : // player selects scissors
     playerSelection === "scissors"
     ? compSelection === "paper"
-      ? (a = "you Win! scissors cuts paper")
+      ? (a = "You Win! scissors cuts paper")
       : compSelection === "rock"
       ? (a = "You lose! rock crushes scissors")
       : (a = "It's a Draw!")
     : // player selects paper
     playerSelection === "paper"
     ? compSelection === "rock"
-      ? (a = "you Win! paper covers rock")
+      ? (a = "You Win! paper covers rock")
       : compSelection === "scissors"
       ? (a = "You lose! scissors cuts paper")
       : (a = "It's a Draw!")
@@ -59,6 +49,8 @@ let gameLogic = (playerSelection, compSelection) => {
   return a;
 };
 
-// call game logic with  player selection and computer selection;
+// call game logic 5 times with  player selection and computer selection;
+for (let i = 0; i < 5; i++) {
+  alert(gameLogic(playerSelection(i + 1), compSelection()));
+}
 // declare the winner with winning reason/phrase
-alert(gameLogic(playerSelection, compSelection));
